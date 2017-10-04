6b257f6270037dbb3202af1f78c9dfa50e759be0
Kion-Smith <kion_smith@hotmail.com>
2017-10-03 20:07:34 -0500

    added setters and getters

--- 2.5 Driving game/src/Entity/player.java
+++ 2.5 Driving game/src/Entity/player.java
@@ -2,9 +2,7 @@ package Entity;
 
 public class player  
 {	
-	private boolean RturningState;
-	private boolean standarState;
-	private boolean LturnState;
+
 	
 	private int xPos;
 	private int yPos;
@@ -12,7 +10,9 @@ public class player
 	private  final int  IDLE =0;
 	private final int TURN = 1;
 	
-	private  final int  actionAmount =2;;
+	private  final int  actionAmount =2;
+	
+	private String fileName;
 	
 	//Some sort of private information for other turning animation
 	private boolean[] carSprite ;
@@ -36,9 +36,42 @@ public class player
 			}
 		}
 	}
+
 	public void setImage()
 	{
-		
+		for(int i =0;i<carSprite.length;i++)
+		{
+			if(carSprite[i] == true)
+			{
+				if(i == IDLE)
+				{
+					fileName = "/idle.png";
+				}
+				else
+				{
+					fileName = "/turing.png";
+				}
+			}
+		}
 	}
 
+	public void setXPos(int x)
+	{
+		xPos = x;
+		
+	}
+	public int getXPos()
+	{
+		return xPos;
+	}
+	
+	public void setYPos(int y)
+	{
+		yPos = y;
+		
+	}
+	public int getYPos()
+	{
+		return yPos;
+	}
 }
