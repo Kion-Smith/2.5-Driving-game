ed36c04a0f565084d3a2c6668a934df446b2e92a
Kion-Smith <kion_smith@hotmail.com>
2017-10-20 23:42:47 -0500

    The maps needed

--- 2.5 Driving game/src/World/projectionPlane.java
+++ 2.5 Driving game/src/World/projectionPlane.java
@@ -1,12 +1,37 @@
 package World;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class projectionPlane 
 {
 	public int[][] level;
 	
-	public projectionPlane()
+	
+	
+	public projectionPlane(String map)
 	{
 		//create level array
+	
+		
+		try
+		{
+			File file = new File("Map1.txt");
+			Scanner sc = new Scanner(file);
+			
+			int row=0;
+			int col =0;
+			while(sc.hasNextLine())
+			{
+				row++;
+				int i = sc.nextInt();
+				level[row][col] = i;
+			}
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 	
 	//add calculations
